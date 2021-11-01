d72040e77d45cf1eafeff4697ea73f67852c38a5
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-11-01 14:38:57 +0200

    project struture

--- src/Primavera.RH.Funcionarios/UI/FDU_FichaFuncionario.Designer.cs
+++ src/Primavera.RH.Funcionarios/UI/FDU_FichaFuncionario.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.btProcessar = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -36,8 +38,7 @@
             this.txtCodigo = new System.Windows.Forms.TextBox();
             this.txtNome = new System.Windows.Forms.TextBox();
             this.txtSalario = new System.Windows.Forms.NumericUpDown();
-            this.btProcessar = new System.Windows.Forms.Button();
-            this.btCancelar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.txtSalario)).BeginInit();
@@ -48,24 +49,45 @@
             this.groupBox1.Controls.Add(this.btCancelar);
             this.groupBox1.Controls.Add(this.btProcessar);
             this.groupBox1.Controls.Add(this.tableLayoutPanel1);
-            this.groupBox1.Location = new System.Drawing.Point(13, 17);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(400, 179);
+            this.groupBox1.Size = new System.Drawing.Size(400, 163);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Dados Funcionarios";
             // 
+            // btCancelar
+            // 
+            this.btCancelar.Location = new System.Drawing.Point(149, 129);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btCancelar.TabIndex = 2;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btProcessar
+            // 
+            this.btProcessar.Location = new System.Drawing.Point(259, 129);
+            this.btProcessar.Name = "btProcessar";
+            this.btProcessar.Size = new System.Drawing.Size(75, 23);
+            this.btProcessar.TabIndex = 1;
+            this.btProcessar.Text = "Processar";
+            this.btProcessar.UseVisualStyleBackColor = true;
+            this.btProcessar.Click += new System.EventHandler(this.btProcessar_Click);
+            // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.73333F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 84.26667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 44F));
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.txtCodigo, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.txtNome, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.txtSalario, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 2, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 20);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
@@ -87,7 +109,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 27);
+            this.label2.Location = new System.Drawing.Point(3, 29);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 1;
@@ -96,7 +118,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 54);
+            this.label3.Location = new System.Drawing.Point(3, 56);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(43, 13);
             this.label3.TabIndex = 2;
@@ -104,48 +126,39 @@
             // 
             // txtCodigo
             // 
-            this.txtCodigo.Location = new System.Drawing.Point(62, 3);
+            this.txtCodigo.Location = new System.Drawing.Point(55, 3);
             this.txtCodigo.Name = "txtCodigo";
-            this.txtCodigo.Size = new System.Drawing.Size(302, 21);
+            this.txtCodigo.Size = new System.Drawing.Size(272, 21);
             this.txtCodigo.TabIndex = 3;
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(62, 30);
+            this.txtNome.Location = new System.Drawing.Point(55, 32);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(302, 21);
+            this.txtNome.Size = new System.Drawing.Size(272, 21);
             this.txtNome.TabIndex = 4;
             // 
             // txtSalario
             // 
-            this.txtSalario.Location = new System.Drawing.Point(62, 57);
+            this.txtSalario.Location = new System.Drawing.Point(55, 59);
             this.txtSalario.Maximum = new decimal(new int[] {
             -727379969,
             232,
             0,
             0});
             this.txtSalario.Name = "txtSalario";
-            this.txtSalario.Size = new System.Drawing.Size(302, 21);
+            this.txtSalario.Size = new System.Drawing.Size(272, 21);
             this.txtSalario.TabIndex = 5;
             // 
-            // btProcessar
+            // button2
             // 
-            this.btProcessar.Location = new System.Drawing.Point(306, 129);
-            this.btProcessar.Name = "btProcessar";
-            this.btProcessar.Size = new System.Drawing.Size(75, 23);
-            this.btProcessar.TabIndex = 1;
-            this.btProcessar.Text = "Processar";
-            this.btProcessar.UseVisualStyleBackColor = true;
-            this.btProcessar.Click += new System.EventHandler(this.btProcessar_Click);
-            // 
-            // btCancelar
-            // 
-            this.btCancelar.Location = new System.Drawing.Point(209, 129);
-            this.btCancelar.Name = "btCancelar";
-            this.btCancelar.Size = new System.Drawing.Size(75, 23);
-            this.btCancelar.TabIndex = 2;
-            this.btCancelar.Text = "Cancelar";
-            this.btCancelar.UseVisualStyleBackColor = true;
+            this.button2.Location = new System.Drawing.Point(333, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "F4";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // FDU_FichaFuncionario
             // 
@@ -154,7 +167,7 @@
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "FDU_FichaFuncionario";
-            this.Size = new System.Drawing.Size(428, 212);
+            this.Size = new System.Drawing.Size(408, 170);
             this.Text = "Ficha Funcionario";
             this.groupBox1.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
@@ -177,5 +190,6 @@
         private System.Windows.Forms.NumericUpDown txtSalario;
         private System.Windows.Forms.Button btCancelar;
         private System.Windows.Forms.Button btProcessar;
+        private System.Windows.Forms.Button button2;
     }
 }
